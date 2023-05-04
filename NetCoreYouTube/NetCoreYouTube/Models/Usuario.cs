b1d910daff5bcf184342e72e265e594cfbb5e221
Rodrigo Concha <rodrigoconcham@gmail.com>
2023-05-04 11:48:11 -0400

    Add files via upload

---
+++ NetCoreYouTube/NetCoreYouTube/Models/Usuario.cs
@@ -0,0 +1,47 @@
+﻿namespace NetCoreYouTube.Models
+{
+    public class Usuario
+    {
+        public string idUsuario { get; set; }
+        public string usuario { get; set; }
+        public string passrowd { get; set; }
+        public string rol { get; set; }
+
+        public static List<Usuario> DB()
+        {
+            var list = new List<Usuario>()
+            {
+                new Usuario
+                {
+                    idUsuario = "1",
+                    usuario = "Mateo",
+                    passrowd = "123.",
+                    rol = "empleado"
+                },
+                new Usuario
+                {
+                    idUsuario = "2",
+                    usuario = "Marcos",
+                    passrowd = "123.",
+                    rol = "empleado"
+                },
+                new Usuario
+                {
+                    idUsuario = "3",
+                    usuario = "Lucas",
+                    passrowd = "123.",
+                    rol = "asesor"
+                },
+                new Usuario
+                {
+                    idUsuario = "4",
+                    usuario = "Juan",
+                    passrowd = "123.",
+                    rol = "administrador"
+                }
+            };
+
+            return list;
+        }
+    }
+}
---
+++ NetCoreYouTube/NetCoreYouTube/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+    // Titulo (Dise�o)
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Software Lion", Version = "v1" });
+
+    //Boton Autorize (Swagger)
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Jwt Authorization",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+       {
+           new OpenApiSecurityScheme
+           {
+               Reference = new OpenApiReference
+               {
+                   Type = ReferenceType.SecurityScheme,
+                   Id = "Bearer"
+               }
+           },
+           new string[] {}
+       }
+    });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
